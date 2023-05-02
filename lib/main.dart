f90a8bfc8a7db4381a7a4a9ec723602a8e1a1c36
Cesar Morales <cesar.aml1891@gmail.com>
2023-05-01 19:39:14 -0500

    Agregando data de subtitulo

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,44 @@ void main() {
   runApp(MyApp());
 }
 
+class Util {
+  static getMonth(int index) {
+    switch (index) {
+      case 1:
+        return "Enero";
+      case 2:
+        return "Febrero";
+      case 3:
+        return "Marzo";
+      case 4:
+        return "Abril";
+      case 5:
+        return "Mayo";
+      case 6:
+        return "Junio";
+      case 7:
+        return "Julio";
+      case 8:
+        return "Agisto";
+      case 9:
+        return "Setiembre";
+      case 10:
+        return "Octubre";
+      case 11:
+        return "Noviembre";
+      case 12:
+        return "Diciembre";
+    }
+  }
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    DateTime now = DateTime.now().toUtc();
+    String subtitulo =
+        "${Util.getMonth(now.month)} ${now.day.toString()} at ${(now.hour).toString()}:${now.minute.toString()}";
+    print(subtitulo);
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white,
@@ -39,6 +74,18 @@ class MyApp extends StatelessWidget {
             ),
           ),
           backgroundColor: Colors.white,
+        ),
+        body: Column(
+          children: [
+            Text(
+              subtitulo,
+            ),
+            const Image(
+              image: AssetImage(
+                "assets/images/noticia.png",
+              ),
+            ),
+          ],
         ),
       ),
     );
